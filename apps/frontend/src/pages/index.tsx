f56392fb191105c37b0a06eee947817e4b4d06dd
Nikita Gizatulin <nikitagizatulin@gmail.com>
2026-01-05 19:25:31 +0200

    minor UI adjustment

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useQuery, useMutation } from "@apollo/client/react";
 import { initializeApollo } from "@/lib/apolloClient";
 import { ARTICLES_QUERY, DELETE_ARTICLE_MUTATION } from "@/graphql/articles";
 import Link from "next/link";
-import { List, Button, Space, Typography, Card, Empty, Modal, message } from "antd";
+import { Row, Col, Button, Space, Typography, Card, Empty, Modal, message } from "antd";
 import { PlusOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import PageLayout from "@/components/PageLayout";
 import LoadingSpinner from "@/components/LoadingSpinner";
@@ -82,7 +82,7 @@ export default function Home() {
             cache.gc();
           },
         });
-        
+
         if (result.data?.deleteArticle) {
           message.success("Article deleted successfully");
         }
@@ -92,7 +92,7 @@ export default function Home() {
   if (loading && !data) {
     return <LoadingSpinner />;
   }
-  
+
   if (!data) {
     return (
       <PageLayout>
@@ -102,69 +102,70 @@ export default function Home() {
   }
 
   const { items, nextCursor } = data.articles;
-  
+
   return (
     <PageLayout maxWidth="1200px">
-        <Space orientation="vertical" size="large" style={{ width: "100%" }}>
-          <Link href="/article/new">
-            <Button type="primary" icon={<PlusOutlined />}>
-              Create article
-            </Button>
-          </Link>
+      <Space orientation="vertical" size="large" style={{ width: "100%" }}>
+        <Link href="/article/new">
+          <Button type="primary" icon={<PlusOutlined />}>
+            Create article
+          </Button>
+        </Link>
 
-          <List
-            grid={{ gutter: 16, xs: 1, sm: 1, md: 2, lg: 2, xl: 2 }}
-            dataSource={items}
-            renderItem={(article: SportsArticle) => (
-              <List.Item>
-                <Card
-                  style={{ width: "100%" }}
-                  actions={[
-                    <Link key="edit" href={`/article/${article.id}/edit`}>
-                      <Button type="link" icon={<EditOutlined />}>
-                        Edit
-                      </Button>
-                    </Link>,
-                    <Button
-                      key="delete"
-                      type="link"
-                      danger
-                      icon={<DeleteOutlined />}
-                      onClick={() => onDelete(article.id)}
-                    >
-                      Delete
-                    </Button>,
-                  ]}
-                >
-                  <Link href={`/article/${article.id}`}>
-                    <Title level={5} ellipsis={{ tooltip: article.title }}>
-                      {article.title}
-                    </Title>
-                  </Link>
-                </Card>
-              </List.Item>
-            )}
-          />
-
-          {nextCursor && (
-            <div style={{ textAlign: "center", marginTop: "16px" }}>
-              <Button
-                type="default"
-                onClick={() =>
-                  fetchMore({
-                    variables: {
-                      limit: 10,
-                      cursor: nextCursor,
-                    },
-                  })
-                }
-                loading={loading}
+        <Row gutter={[16, 16]}>
+          {items.map((article: SportsArticle) => (
+            <Col key={article.id}
+              xs={24}
+              md={12}
+            >
+              <Card
+                style={{ width: "100%" }}
+                actions={[
+                  <Link key="edit" href={`/article/${article.id}/edit`}>
+                    <Button type="link" icon={<EditOutlined />}>
+                      Edit
+                    </Button>
+                  </Link>,
+                  <Button
+                    key="delete"
+                    type="link"
+                    danger
+                    icon={<DeleteOutlined />}
+                    onClick={() => onDelete(article.id)}
+                  >
+                    Delete
+                  </Button>,
+                ]}
               >
-                Load more
-              </Button>
-            </div>
-          )}
-        </Space>
+                <Link href={`/article/${article.id}`}>
+                  <Title level={5} ellipsis={{ tooltip: article.title }}>
+                    {article.title}
+                  </Title>
+                </Link>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+
+        {nextCursor && (
+          <div style={{ textAlign: "center", marginTop: "16px" }}>
+            <Button
+              type="default"
+              onClick={() =>
+                fetchMore({
+                  variables: {
+                    limit: 10,
+                    cursor: nextCursor,
+                  },
+                })
+              }
+              loading={loading}
+            >
+              Load more
+            </Button>
+          </div>
+        )}
+      </Space>
     </PageLayout>
   );
 }
